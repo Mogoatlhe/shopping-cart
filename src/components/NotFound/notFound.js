2a4f51da5b886698dec6c2629217172d98784f14
mogoatlhe <iam@ofent.se>
2022-06-27 21:11:12 +0200

    create the not found component

---
+++ src/components/NotFound/notFound.js
@@ -0,0 +1,11 @@
+import Nav from "../Nav/nav";
+
+const NotFound = () => {
+  return (
+    <div id="not-found-container" className="container">
+      <Nav page="not-found" />
+    </div>
+  );
+};
+
+export default NotFound;
