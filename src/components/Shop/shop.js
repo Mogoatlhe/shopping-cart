ad5d96ded1b7a4f484bd7cba1280499dcb46d99e
mogoatlhe <iam@ofent.se>
2022-06-27 17:02:59 +0200

    add the shop componet

---
+++ src/components/Shop/shop.js
@@ -0,0 +1,11 @@
+import Nav from "../Nav/nav";
+
+const Shop = () => {
+  return (
+    <div id="shop-container" className="container">
+      <Nav page="shop" />
+    </div>
+  );
+};
+
+export default Shop;
