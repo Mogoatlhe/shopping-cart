2d5d83047e5024926bea368a16bd90b57c44c477
mogoatlhe <iam@ofent.se>
2022-06-28 13:42:42 +0200

    display the home page image

--- src/components/Home/brief.js
+++ src/components/Home/brief.js
@@ -1,7 +1,13 @@
+import image from "../../images/white-all-star.avif";
+
 const Brief = () => {
   return (
     <div id="brief-container">
-      <p></p>
+      <img
+        src={image}
+        alt="low top white converse shoes"
+        id="home-page-image"
+      />
     </div>
   );
 };
