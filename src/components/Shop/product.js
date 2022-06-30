14b4c1d94a5708f7237dbe0534831dff6fc97149
mogoatlhe <iam@ofent.se>
2022-06-30 15:07:37 +0200

    display "add to to cart" button

--- src/components/Shop/product.js
+++ src/components/Shop/product.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const Product = ({ shoeData }) => {
+  const [cartCount, setCartCount] = useState(0);
+
+  const incrementCartCount = () => {
+    setCartCount(cartCount + 1);
+  };
+
+  const decrementCartCount = () => {
+    setCartCount(cartCount - 1);
+  };
+
   return (
     <div className="product-container">
       <img
@@ -10,6 +22,15 @@ const Product = ({ shoeData }) => {
         {shoeData.name}
         <span>R{shoeData.price}</span>
       </p>
+      <div className="product-cart-container">
+        <button className="add-to-cart" onClick={incrementCartCount}>
+          +
+        </button>
+        <div className="cart-count">{cartCount}</div>
+        <button className="remove-from-cart" onClick={decrementCartCount}>
+          -
+        </button>
+      </div>
     </div>
   );
 };
