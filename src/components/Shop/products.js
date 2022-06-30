1c0d8eef20a9a7cff53a6815a67b6fa8c06bf351
mogoatlhe <iam@ofent.se>
2022-06-30 08:10:41 +0200

    add products component

---
+++ src/components/Shop/products.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const Products = () => {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    let imageLink = `http://assets.superbalistcdn.co.za/`;
+
+    const changeBaseUrl = (response) => {
+      return response.search.data.map((data) => {
+        data.assets = data.assets.map((image) => {
+          image.base_url = `${imageLink}${data.asset.base_url.slice(-20)}`;
+          return image;
+        });
+        return data;
+      });
+    };
+
+    const fetchData = async () => {
+      let response = await fetch(
+        "https://superbalist.com/api/public/es/catalogue?brand=converse&department=men&category=shoes&route=%7B%22path%22:%22%2Fbrands%2Fconverse%2Fmen%2Fshoes%22%7D"
+      );
+
+      response = await response.json();
+      const data = changeBaseUrl(response);
+
+      if (productsData.length === 0) setProductsData(data);
+    };
+
+    fetchData();
+  }, [productsData]);
+};
+
+export default Products;
